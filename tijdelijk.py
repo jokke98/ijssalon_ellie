6cf30f45f8de10a5ffec9e50d1dc4fb168cd4fa2
Jonas Van Es <jonas.vanes@hotmail.com>
2025-10-03 00:21:59 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,14 @@
+Prijzen={"aardbei":"3","vanille":"4","chocolade":"5"}
+aanbieding=(3*0.8)
+reclame_tekst="Vandaag in de aanbieding: vanille-ijs, 1 liter-slechts €"
+reclame_tekst2=(2.40)
+reclame_tekst3="Vandaag in de aanbieding: vanille-ijs, 1 liter-slechts €2.40"
+reclame_tekst4=["Vandaag in de aanbieding: vanille-ijs, 1 liter-slechts €2.40"]
+keuze=reclame_tekst4[0]
+el="Vandaag in de aanbieding: vanille-ijs, 1 liter-slechts €2.40"
+for i in el:
+    print(i)
+print("el".lower)
+for i in el:
+    if len (el>5):
+        print("el").upper()
